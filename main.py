d09525241043826c63e6013e5ac2689ebd2c875b
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-07-08 22:42:03 +0000

    Add title for the chart

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ temp = [28,32,31,40,45,55,60,65,54,43,34,30]
 plt.plot(month, temp, color="purple")
 
 plt.xlabel("Month", fontsize=16)
-
 plt.ylabel("Temperature", fontsize=16)
+
+plt.title("Average Temperature in July, 2018", fontsize=20)
